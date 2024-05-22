3bbffcae35252e629c50d127a88825d512f2c66c
Bikram Shrestha <stha.bikram999@gmail.com>
2024-05-23 00:46:14 +0545

    Updating an existing Note

--- routes/notes.js
+++ routes/notes.js
@@ -7,7 +7,7 @@ const {body,validationResult}=require('express-validator')
 // Route 1: Get ALl the Notes using : Get "api/notes/fetchallnotes" Login Required
 router.get('/fetchallnotes', fetchuser, async (req, res) => {
     try{
-    const notes = await Notes.find({user: req.user.id})
+    const notes = await Note.find({user: req.user.id})
     res.json(notes); 
     }
     catch(error){
@@ -18,7 +18,7 @@ router.get('/fetchallnotes', fetchuser, async (req, res) => {
 });
 
 // Route 1: Add a new Note using : Post "api/notes/addnote" Login Required
-router.get('/addnote', fetchuser, [
+router.post('/addnote', fetchuser, [
     body('title','Enter a valid title').isLength({ min:3 }),
     body('description', 'Description must me at least 5 letter').isLength({ min:5 }),
 ],
@@ -42,6 +42,35 @@ router.get('/addnote', fetchuser, [
         }
 });
 
+// Route 3: Updating a existing Note using : Post "api/notes/updatenote" Login Required
+router.put('/updatenote/:id', fetchuser, [
+    body('title','Enter a valid title').isLength({ min:3 }),
+    body('description', 'Description must me at least 5 letter').isLength({ min:5 }),
+], async (req, res) => {
+    try{
+    const {title ,description,tag}= req.body
+
+    //Create a newNote Object
+    const newNote ={};
+    if(title){newNote.title =title};
+    if(description){newNote.description =description};
+    if(tag){newNote.tag =tag};
+
+    //Find the note to be updated and update it
+    let note = await Note.findById(req.params.id);
+    if(!note){return res.Status(404).send("Not FOund")}
+
+    if(note.user.toString() != req.user.id){
+        return res.status(401).send("Not Allowed");
+    }
+
+    note=await Note.findByIdAndUpdate(req.params.id, {$set: newNote}, {new:true})
+    res.json({note});
+}catch(error){
+    console.error(error.message);
+     res.status(500).send("some Error occured");
+}
+});
 
 
 module.exports = router;
